cf62cdaf4232ec6430c10d8ecea4739b218312b0
latifa176 <latifa176@users.noreply.github.com>
2020-08-29 19:40:01 +0300

    Final style changes + enhanced readability

--- script.php
+++ script.php
@@ -5,9 +5,6 @@
   </head>
   <body>
   <?php include 'submittedformpage.html';
-// define variables and set to empty values
-$nameErr = $emailErr =$nationalityErr=$universityErr=$majorErr= $mobileErr=$genderErr =$programErr= $yearErr =$gradErr = "";
-$name = $email = $gender = $university= $nationality=$comment =$mobile=$major=$program=$year =$grad= "";
 $name = test_input($_POST["name"]);
 $nationality = test_input($_POST["nationality"]);
 $university = test_input($_POST["university"]);
@@ -27,6 +24,7 @@ $program = test_input($_POST["program"]);
 
 function OpenCon()
  {
+   //Change database host, user and password down here 
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "l44443333";
@@ -67,7 +65,7 @@ function CloseCon($conn)
                     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){echo "<script>alert('Incorrect email format. Please enter a correct email'); window.location.href='formpage.html'; </script>"; exit("Please enter a correct email");}
                     if(!empty($mobile)){
                       //mobile is not empty
-                      if(!preg_match("/^[0-9 ]*$/",$mobile)){echo "<script>alert('Please enter your mobile number. Only numbers allowed'); window.location.href='formpage.html'; </script>"; exit("Please enter your mobile number. Only numbers allowed");}
+                      if(!preg_match("/^[0-9 ]*$/",$mobile OR strlen($mobile)<10)){echo "<script>alert('Please enter your mobile number. Only numbers allowed'); window.location.href='formpage.html'; </script>"; exit("Please enter your mobile number. Only numbers allowed");}
                       if(!empty($program)){
                         //program code is not empty
                         $conn = OpenCon();
@@ -95,113 +93,6 @@ function CloseCon($conn)
   } else { echo '<script>alert("Please enter your name"); window.location.href="formpage.html";</script>';}
 
  }
-/*
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
-    }
-  }
-
-  if (empty($_POST["nationality"])) {
-    $nationalityErr = "nationality is required";
-  } else {
-    $nationality = test_input($_POST["nationality"]);
-    // check if nationality address is well-formed
-    if (!preg_match("/^[a-zA-Z ]*$/",$nationality)) {
-        $nationalityErr = "Only letters and white space allowed";
-    }
-  }
-
-  if (empty($_POST["university"])) {
-    $universityErr = "university is required";
-  } else {
-    $university = test_input($_POST["university"]);
-    // check if university only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$university)) {
-      $universityErr = "Only letters and white space allowed";
-    }
-  }
-
-  if (empty($_POST["major"])) {
-    $majorErr = "major is required";
-  } else {
-    $university = test_input($_POST["major"]);
-    // check if major only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$major)) {
-      $majorErr = "Only letters and white space allowed";
-    }
-  }
-
-  if (empty($_POST["year"])) {
-    $yearErr = "year is required";
-  } else {
-    $year = test_input($_POST["year"]);
-    // check if year only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$year)) {
-      $yearErr = "Only letters and white space allowed";
-    }
-  }
-
-  if (empty($_POST["mobile"])) {
-    $mobileErr = "mobile is required";
-  } else {
-    $mobile = test_input($_POST["mobile"]);
-    // check if year only contains letters and whitespace
-    if (!preg_match("/^[0-9 ]*$/",$mobile)) {
-      $mobileErr = "Only number allowed";
-    }
-  }
-
-
-  if (empty($_POST["program"])) {
-    $programErr = "program is required";
-  } else {
-    $program = test_input($_POST["program"]);
-    // check if year only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$program)) {
-      $programErr = "Only  letters and white space allowed";
-    }
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
-
-if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-
-
-if (empty($_POST["grad"])) {
-    $gradErr = "grad is required";
-  } else {
-    $grad = test_input($_POST["grad"]);
-  }
-
-
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }*/
-
-  
 
   function test_input($data) {
     $data = trim($data);
